01b9f4d3a82cb7730449b75098da112ff651350d
ARCADEGC <JrTrnka55@gmail.com>
2023-04-14 21:40:25 +0200

    UPDATE

---
+++ radToDeg.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+float degToRad(float x){
+    x = x * 0.0174532925;
+    return x;
+}
+
+float radToDeg(float x){
+    x = x / 0.0174532925;
+    return x;
+}
+
+int main(){
+    for(float i = 0; i <= 360; i += 30){
+        printf("%f; %f; %f; %f \n", i, degToRad(i), sin(degToRad(i)), cos(degToRad(i)));
+    }
+    return 0;
+}
